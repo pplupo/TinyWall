e122f816b48a96137075c161f3dcf36881863482
Shiraz Adam <93381720+ShirazAdam@users.noreply.github.com>
2025-02-18 21:24:30 +0000

    added creation date column to settings

--- TinyWall/SettingsForm.cs
+++ TinyWall/SettingsForm.cs
@@ -238,7 +238,8 @@ namespace pylorak.TinyWall
                 default:
                     throw new NotImplementedException();
             }
-
+			
+            li.SubItems.Add(ex.CreationDate.ToString("yyyy/MM/dd HH:mm"));
 
             if (ex.Policy.PolicyType == PolicyType.HardBlock)
             {
